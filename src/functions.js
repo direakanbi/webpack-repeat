3006e90d97c807b94464068817e092861d5d9340
direakanbi <83509786+direakanbi@users.noreply.github.com>
2021-12-01 10:19:55 +0100

    Added interactive list features

--- src/functions.js
+++ src/functions.js
@@ -1,10 +1,9 @@
-export default function displaylist(todos, list) {
-  todos.forEach((task) => {
-    const li = document.createElement('li');
-    const text = `<div class="list-container"> <input type='checkbox' class='check-input' value='${task.completed}' aria-label='...'>
-    <p class="todo-text">${task.description}</p><a class="del-menu" href="#"><i class="fas fa-ellipsis-v"></i></a></div>`;
-    li.classList.add('list-item');
-    li.innerHTML = text;
-    list.appendChild(li);
-  });
-}
+const setToLocalStorage = (toDo) => {
+  console.log(toDo);
+  localStorage.setItem('toDo', JSON.stringify(toDo));
+};
+const getFromLocalStorage = () => {
+  const toDo = localStorage.getItem('toDo');
+  return JSON.parse(toDo);
+};
+export { setToLocalStorage, getFromLocalStorage };
