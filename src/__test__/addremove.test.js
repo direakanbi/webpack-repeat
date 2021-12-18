a05e22953f328906e41bdccdad3edaa4edab041f
direakanbi <direakanbi@icloud.com>
2021-12-18 08:47:58 +0100

    Fixed Linter Errors

--- src/__test__/addremove.test.js
+++ src/__test__/addremove.test.js
@@ -1,10 +1,10 @@
-import add  from '../__mocks__/add';
+import add from '../__mocks__/add';
 import removeItem from '../__mocks__/removeitem';
 
 jest.mock('../functions');
 jest.mock('../storage');
 jest.mock('../addremove');
-jest.mock('../index')
+jest.mock('../index');
 
 describe('Add function', () => {
   test('Add one new item to the list', () => {
@@ -14,4 +14,4 @@ describe('Add function', () => {
   test('Remove one new item from the list', () => {
     expect(removeItem().length).toBe(0);
   });
-})
+});
