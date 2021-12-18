3ecbf952a372e9587c8441651868f5a96f90f6ae
direakanbi <direakanbi@icloud.com>
2021-12-18 10:19:46 +0100

    Adde corresponding import statements

--- src/__test__/addremove.test.js
+++ src/__test__/addremove.test.js
@@ -1,5 +1,10 @@
 import add from '../__mocks__/add';
 import removeItem from '../__mocks__/removeitem';
+import editItem from '../__mocks__/editItem';
+import indexUpdate from '../__mocks__/indexUpdate';
+import clearAll from '../__mocks__/clearAll';
+import status from '../__mocks__/status';
+
 
 jest.mock('../functions');
 jest.mock('../storage');
@@ -26,7 +31,7 @@ describe('Add function', () => {
   test('Update the index of list when one item is deleted', () => {
     expect(indexUpdate()).toEqual([1, 2]);
   });
-  
+
   test('Check list item when status changes', () => {
     expect(status()).toBe(true);
   });
